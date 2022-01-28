4cec0f812f6d203299e1b84f68abcdc40f27d4cb
vamidi <valencio_masaki16@live.nl>
2022-01-28 14:10:39 +0100

    Release 2.1.0 - Depenedency upgrades. code upgrade.

--- screenres.cc
+++ screenres.cc
@@ -12,18 +12,17 @@
 #include "headers.h"
 
 using v8::Local;
-using v8::Handle;
 using v8::Object;
 using v8::Array;
 using v8::Number;
 using v8::String;
 using v8::FunctionTemplate;
 
-void init(Handle<Object>);
+void init(Local<Object>);
 
 NAN_METHOD(set) {
   Nan::HandleScope scope;
-  int code = changeRes(info[0]->Uint32Value(), info[1]->Uint32Value());
+  int code = changeRes(info[0]->Uint32Value(Nan::GetCurrentContext()).FromJust(), info[1]->Uint32Value(Nan::GetCurrentContext()).FromJust());
 
   if (!code) {
     info.GetReturnValue().SetUndefined();
@@ -36,14 +35,14 @@ NAN_METHOD(get) {
   Nan::HandleScope scope;
   Resolution res = fetchRes();
   Local<Array> arr = Nan::New<Array>(2);
-  arr->Set(0, Nan::New<Number>(res.width));
-  arr->Set(1, Nan::New<Number>(res.height));
+  arr->Set(Nan::GetCurrentContext(), 0, Nan::New<Number>(res.width));
+  arr->Set(Nan::GetCurrentContext(), 1, Nan::New<Number>(res.height));
   info.GetReturnValue().Set(arr);
 }
 
 NAN_MODULE_INIT (init) {
-  Nan::Set(target, Nan::New<String>("set").ToLocalChecked(), Nan::New<FunctionTemplate>(set)->GetFunction());
-  Nan::Set(target, Nan::New<String>("get").ToLocalChecked(), Nan::New<FunctionTemplate>(get)->GetFunction());
+  Nan::Set(target, Nan::New<String>("set").ToLocalChecked(), Nan::New<FunctionTemplate>(set)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
+  Nan::Set(target, Nan::New<String>("get").ToLocalChecked(), Nan::New<FunctionTemplate>(get)->GetFunction(Nan::GetCurrentContext()).ToLocalChecked());
 }
 
 NODE_MODULE(screenres, init);
